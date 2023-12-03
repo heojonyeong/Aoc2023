cad15d94afd0a7a4893b4ebc0e4158576cc560f0
Me <me@me.org>
2023-12-03 21:17:36 +0100

    Day 3 part 2

--- n3/main.hs
+++ n3/main.hs
@@ -1,5 +1,6 @@
 import qualified Common
 import Data.Array
+import Data.List
 
 (!?) :: (Ix i) => Array i e -> i -> Maybe e
 a !? idx = if inRange (bounds a) idx
@@ -29,10 +30,61 @@ windowingHelper arr idx@(idx1, idx2) cur total foundSymbol | stop = total
           stop = not (inRange (bounds arr) idx)
           nowAsNum :: Integer = read [now]
 
+findLeftMostDigitIdx :: Array (Int, Int) Char -> (Int, Int) -> (Int, Int)
+findLeftMostDigitIdx arr (i,j) = case arr !? (i, j-1) of
+                                    Just e -> if Common.isDigit e
+                                              then findLeftMostDigitIdx arr (i, j-1)
+                                              else (i, j)
+                                    _ -> (i,j) 
+
+-- find all Numbers around a certain index
+findNumbersAround :: Array (Int, Int) Char -> (Int, Int) -> [Integer]
+findNumbersAround arr (idx1,idx2) = filter (/=0) numbers 
+    where startIdx = [(i,j) | i <- [idx1-1, idx1, idx1+1], j <- [idx2 -1, idx2, idx2+1]]
+          filtered :: [(Int,Int)] -> [(Int,Int)]
+          filtered [] = []
+          filtered (x:xs) = case arr !? x of
+                              Just e -> if Common.isDigit e
+                                        then x : filtered xs
+                                        else filtered xs
+                              _ -> filtered xs
+          leftMostDigits = map head (group $ sort $  map (findLeftMostDigitIdx arr) (filtered startIdx))
+          numbers = map (numAt arr) leftMostDigits
+
+
+numAt :: Array (Int, Int) Char -> (Int, Int) -> Integer
+numAt arr idx@(idx1, idx2) = helper 0 (idx1, idx2)
+    where helper :: Integer -> (Int, Int) -> Integer
+          helper total (idx1,idx2) = case arr !? (idx1,idx2) of
+                                        Just e -> if Common.isDigit e
+                                                   then helper (total*10 + read [e]) (idx1,idx2+1)
+                                                   else total
+                                        _ -> total
+
 solve1 :: Array (Int, Int) Char -> Integer
 solve1 x = windowingHelper x (0,0) 0 0 False
 
+solve2Helper :: Array (Int, Int) Char -> (Int, Int) -> Integer -> Integer
+solve2Helper arr idx@(idx1,idx2) total | stop = total
+                                       | isStar = case numsAround of
+                                                   [a,b] -> solve2Helper arr (next idx) total + a*b
+                                                   _ -> solve2Helper arr (next idx) total
+                                       | otherwise = solve2Helper arr (next idx) total
+    where now = arr ! idx
+          isStar = now == '*'
+          numsAround = findNumbersAround arr idx
+          rowEndIdx = snd (snd (bounds arr))
+          atEnd = idx2 == rowEndIdx
+          stop = not (inRange (bounds arr) idx)
+          next (x,y) = if atEnd
+                       then (x+1, 0)
+                       else (x, y+1)
+
+solve2:: Array (Int, Int) Char -> Integer
+solve2 arr = solve2Helper arr (fst (bounds arr)) 0
+
 main = do 
-        splitLines <- Common.readAndSplit "input_sample.txt"
+        splitLines <- Common.readAndSplit "input.txt"
         let arr = Common.to2DArray splitLines
         print(solve1 arr)
+        print(solve2 arr)
