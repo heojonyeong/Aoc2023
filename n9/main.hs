56d1169581a4dc478ecca05e83c42dd100e86bd1
Me <me@me.org>
2023-12-09 15:11:56 +0100

    Day 9 part 2

--- n9/main.hs
+++ n9/main.hs
@@ -1,28 +1,32 @@
 import qualified Common
 
-buildInitial :: String -> [Integer]
-buildInitial = reverse . (map read . Common.splitAtMultiple ' ')
+buildInitial :: ([Integer] -> [Integer]) -> String -> [Integer]
+buildInitial transform = transform . (map read . Common.splitAtMultiple ' ')
 
-buildStack :: String -> [[Integer]]
-buildStack x = buildStack_ [initial]
+buildStack :: ([Integer] -> [Integer]) -> String -> [[Integer]]
+buildStack transform x = buildStack_ [initial]
 
-    where initial = buildInitial x
+    where initial = buildInitial transform x
           buildStack_ :: [[Integer]] -> [[Integer]]
           buildStack_ (y:ys) = if done then (y:ys) else buildStack_ (diffList : (y:ys))
             where done = all (==0) y      
-                  diffList = map (\a -> fst a - snd a) (zip y (tail y))
+                  diffList = map (uncurry (-)) (zip y (tail y))
 
-solveStack :: String -> Integer
-solveStack x = solveStack_ stack
-    where stack = buildStack x
+solveStack :: ([Integer] -> [Integer]) -> String -> Integer
+solveStack transform x = solveStack_ stack
+    where stack = buildStack transform x
           
           solveStack_ :: [[Integer]] -> Integer
           solveStack_ y = sum $ map head y
 
 solve1 :: [String] -> Integer
-solve1 x = sum $ map solveStack x
+solve1 x = sum $ map (solveStack reverse) x
+
+solve2 :: [String] -> Integer
+solve2 x = sum $ map (solveStack id) x
 
 main = do 
         file <- getLine
         splitLines <- Common.readAndSplit file
         print $ solve1 splitLines
+        print $ solve2 splitLines
