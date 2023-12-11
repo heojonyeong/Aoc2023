8df5c5905cb33392f5916cbdde77e72202570147
Me <me@me.org>
2023-12-11 19:02:27 +0100

    Day 11 part 2

--- n11/main.hs
+++ n11/main.hs
@@ -13,27 +13,27 @@ findGalaxies x = findGalaxies_ x 0
     where findGalaxies_ [] _ = []
           findGalaxies_ (x:xs) idx = zip (repeat idx) (findGalaxiesLine x) ++ findGalaxies_ xs (idx+1)
 
-expandX :: [(Int,Int)] -> [(Int,Int)]
-expandX x = expandX_ sorted 0 (0,0)
+expandX :: Int -> [(Int,Int)] -> [(Int,Int)]
+expandX factor x = expandX_ (factor-1) sorted 0 (0,0)
     where sorted = Data.List.sortBy comp x
           comp (_,x1) (_,x2) = compare x1 x2
 
-          expandX_ :: [(Int,Int)] -> Int -> (Int,Int) -> [(Int,Int)]
-          expandX_ [] _ _ = []
-          expandX_ ((y,x):xs) adder (_,lx) = if diff > 0
-                                             then (y,x+adder+diff): expandX_ xs (adder+diff) (y,x)
-                                             else (y,x+adder): expandX_ xs adder (y,x)  
+          expandX_ :: Int -> [(Int,Int)] -> Int -> (Int,Int) -> [(Int,Int)]
+          expandX_ _ [] _ _ = []
+          expandX_ factor ((y,x):xs) adder (_,lx) = if diff > 0 
+                                                    then (y,x+adder+diff*factor): expandX_ factor xs (adder+diff*factor) (y,x)
+                                                    else (y,x+adder): expandX_ factor xs adder (y,x)  
             where diff = x - lx - 1
 
-expandY :: [(Int,Int)] -> [(Int,Int)]
-expandY x = expandY_ sorted 0 (0,0)
+expandY :: Int -> [(Int,Int)] -> [(Int,Int)]
+expandY factor x = expandY_ (factor-1) sorted 0 (0,0)
     where sorted = Data.List.sortBy comp x
           comp (y1,_) (y2,_) = compare y1 y2
 
-          expandY_ [] _ _ = []
-          expandY_ ((y,x):xs) adder (ly,_) = if diff > 0
-                                             then (y+diff+adder,x): expandY_ xs (adder+diff) (y,x)  
-                                             else (y+adder, x): expandY_ xs adder (y,x)
+          expandY_ _ [] _ _ = []
+          expandY_ factor ((y,x):xs) adder (ly,_) = if diff > 0
+                                                    then (y+diff*factor+adder,x): expandY_ factor xs (adder+diff*factor) (y,x)  
+                                                    else (y+adder, x): expandY_ factor xs adder (y,x)
             where diff = y - ly - 1
 
 distance :: ((Int,Int), (Int,Int)) -> Int
@@ -43,10 +43,17 @@ unorderedPairs :: [(Int,Int)] -> [((Int,Int), (Int,Int))]
 unorderedPairs [] = []
 unorderedPairs (x:xs) = zip (repeat x) xs ++ unorderedPairs xs
 
+solve_ :: Int -> Int -> [String] -> Int
+solve_ facX facY x = sum $ map distance (unorderedPairs $ expandY facY $ expandX facX $ findGalaxies x)
+
 solve1 :: [String] -> Int
-solve1 x = sum $ map distance (unorderedPairs $ expandY $ expandX $ findGalaxies x)
+solve1 = solve_ 2 2
+
+solve2 :: [String] -> Int
+solve2 = solve_ 1000000 1000000
 
 main = do
     file <- getLine
     splitLines <- Common.readAndSplit file
     print $ solve1 splitLines
+    print $ solve2 splitLines
